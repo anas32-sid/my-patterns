2b8582091489e771a88f9fd4a2fc0bcafde41d05
mohd. anas <anas9650731343@gmail.com>
2025-10-26 01:54:44 +0530

    Add files via upload

---
+++ pattern16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int pattern16(int n){
+    for(int i=0; i<=n; i++){
+        char j='A'+i;
+        for(int k=0; k<=i; k++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n=5;
+    pattern16(n);
+}
